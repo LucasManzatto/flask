d0f74a9ddedc2419d8af89f2a5ce7857249fb526
Lucas Manzatto <lucasmanzatto21@gmail.com>
2019-06-03 16:39:18 -0300

    default parameters mudado para outro arquvio

--- frontend/src/app/shared/parameters.ts
+++ frontend/src/app/shared/parameters.ts
@@ -1,2 +1,12 @@
+import { DefaultQuery } from './models/query.model';
+
 export const DEBOUNCE_TIME = 600;
 export const PAGE_SIZES = [10, 20, 30, 40, 50];
+
+export const DEFAULT_PARAMETERS: DefaultQuery = {
+    direction: 'ASC',
+    page: '1',
+    per_page: '10',
+    query_all: '',
+    sort_column: 'id'
+};
